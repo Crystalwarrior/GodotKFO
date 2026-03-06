ce244593a586b0c39239c33678a5f42b7a2ce61a
Alex Noir <varsash@gmail.com>
2026-03-06 03:04:23 +0300

    Tweak chardisplay test with aspect ratio container being positioned independent of ui

--- _test/char_converter/charDisplay.gd
+++ _test/char_converter/charDisplay.gd
@@ -1,7 +1,7 @@
-extends Node2D
+extends AspectRatioContainer
 
-@onready var aspect_ratio_container: AspectRatioContainer = $".."
 @onready var preview_texture_rect: TextureRect = %PreviewTextureRect
+@onready var preview_node: Node2D = %PreviewNode
 
 @export var preview_height: float = 1.0
 
@@ -11,17 +11,17 @@ func _set_aspect():
 	var VP_Rect = get_viewport_rect()
 	var aspect = VP_Rect.size.x / VP_Rect.size.y
 
-	aspect_ratio_container.ratio = aspect
+	self.ratio = aspect
 	calc_preview_height()
 
 func calc_preview_height():
-	var area = aspect_ratio_container.get_rect()
+	var area = self.get_rect()
 	var base_position
 	preview_texture_rect.size.x = area.size.x
 	preview_texture_rect.size.y = preview_height * area.size.y
 	preview_texture_rect.position = -preview_texture_rect.size / 2
 	base_position = Vector2(area.size.x/2, area.size.y - (preview_texture_rect.size.y/2))
-	position = base_position + (area.size * position_offset_normal)
+	preview_node.position = base_position + (area.size * position_offset_normal)
 
 func _on_aspect_ratio_container_item_rect_changed() -> void:
 	_set_aspect()
